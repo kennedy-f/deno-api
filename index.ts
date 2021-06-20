414147a20bcd3c7224eebc806ef62f77be5c3d3f
kennedy <kennedy2000silva@hotmail.com>
2021-06-20 18:56:55 -0300

    add a comment

--- index.ts
+++ index.ts
@@ -3,5 +3,6 @@ const port = 8080;
 const listener = Deno.listen({ hostname, port });
 console.log(`Listening on ${hostname}:${port}`);
 for await (const conn of listener) {
-    Deno.copy(conn, conn);
+    Deno.copy(conn, conn); // test
+
 }
